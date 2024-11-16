28048a1f481306a0f5e52bf22327b8e474aca3fb
Anjaney Naik <anjaneynaik05@gmail.com>
2024-11-16 12:01:31 -0500

    initial

---
+++ hello.py
@@ -0,0 +1,17 @@
+print("Hello Every one!")
+name = "Anjaney"
+age = 19
+print("My name is" ,name,"and my age is :" ,age)
+print("I am going to explain a new topic:")
+
+#User input 
+
+city = (input("city : "))
+print("your city is :" , city)
+
+college = (input("your address is :"))
+print("your from :" , college)
+
+marks = int (input("Enter your marks :"))
+print("your 12th marks is :" ,marks)
+
