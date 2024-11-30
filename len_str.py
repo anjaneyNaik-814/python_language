3f7eeb96691999e532d992f4d0baa3923ca7cbe2
Anjaney Naik <anjaneynaik05@gmail.com>
2024-11-30 13:57:24 -0500

    initial commit

---
+++ len_str.py
@@ -0,0 +1,9 @@
+sen = "Hello World"
+
+print(len(sen)) # to print length of the string
+
+print(sen[6]) #to access specific world in the string
+
+print(sen[1:5]) #Slicing
+
+print(sen[-5:-1]) #slicing negetive index
