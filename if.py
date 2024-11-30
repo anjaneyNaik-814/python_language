974ca96cfb45d680d919a49b21c98080cdddd61f
Anjaney Naik <anjaneynaik05@gmail.com>
2024-11-30 01:28:14 -0500

    initial commit

---
+++ if.py
@@ -0,0 +1,14 @@
+color = (input("Enter one traffic color :"))
+
+if(color == "red"):
+    print("Stop the vehicle")
+
+elif(color == "yellow") :
+    print("Start the vehicle")
+
+elif(color == "green"):
+    print("Run the vehicle")
+
+else:
+    print("The light is broken")
+    
