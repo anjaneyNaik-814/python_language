6306a164cd250cda2669b30c0c8dbaeec8b00f55
Anjaney Naik <anjaneynaik05@gmail.com>
2024-12-01 00:48:19 -0500

    initial commit

---
+++ nested_dict.py
@@ -0,0 +1,16 @@
+
+#nested dictionary
+
+student ={
+    "name" : "Anjaney",
+    "subject" :{
+        "math" :98,
+        "CC" :78,
+        "PP" :97,
+    }
+}
+
+print(student)
+print(student["subject"])
+print(student["subject"]["PP"])
+
