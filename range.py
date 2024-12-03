80dbdc29ee36e241da40b1b757744d2b7d59b125
Anjaney Naik <anjaneynaik05@gmail.com>
2024-12-03 13:49:00 -0500

    initial commit

---
+++ range.py
@@ -0,0 +1,16 @@
+
+for val in range(10): #(Step)
+    print(val)
+
+print("end loop")
+
+#print even numbers
+
+for i in range(0,10,2) :  #(start, stop , Step)
+    print(i)
+print("end even loop")
+
+#print odd numbers
+
+for i in range(1,10,2) :  
+    print(i)
