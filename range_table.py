aee92372f8a50e58e9d7a3396e265317db666bc2
Anjaney Naik <anjaneynaik05@gmail.com>
2024-12-04 00:50:12 -0500

    initial commit

---
+++ range_table.py
@@ -0,0 +1,21 @@
+for i in range(2,21,2):
+    print(i)
+
+print("loop end")
+
+for i in range(3,31,3):
+    print(i)
+
+print("loop end")
+
+for i in range(4,41,4):
+    print(i)
+
+
+
+#another method of print a taable from user 
+
+n = int(input("enter the number : "))
+
+for i in range(1,11):
+    print(i*n)
