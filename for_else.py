75c720a81dd3cb5485f39f0f04a13100ae352594
Anjaney Naik <anjaneynaik05@gmail.com>
2025-03-15 10:37:21 -0400

    initial commit

--- for_else.py
+++ for_else.py
@@ -1,5 +1,5 @@
-for i in range(6):   #same for while loop when loop is run completly without any break in that case else statement execute hoga
-    if i ==4:
+for i in range(6):   #same for while loop when loop is run completly without any break in that case else statement execute hoga 
+    if i ==5:
         break
     print(i)
 
