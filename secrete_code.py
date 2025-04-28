7000de3e1a4b748489115be796454558310a04e3
Anjaney Naik <anjaneynaik05@gmail.com>
2025-04-28 18:10:38 +0530

    initial commit

---
+++ secrete_code.py
@@ -0,0 +1,33 @@
+st = input("Enter a message : ")
+words = st.split(" ")
+coding = input("Enter 1 for coding and 0 for decoding : ")
+coding = True if (coding=="1") else False
+
+if (coding):
+    nwords=[]
+    for word in words:
+
+        if (len(st)>=3):
+            r1="fjq"
+            r2="oet"
+            stnew=r1 + word[1:] + word[0] +r2
+            nwords.append(stnew)
+
+        else:
+            nwords.append(word[::-1])
+
+    print(" ".join(nwords))
+
+
+else:
+    nwords=[]
+
+    for word in words:
+
+        if(len(word)>=3):
+            stnew = word[3:-3]
+            stnew = stnew[-1] + stnew[:-1]
+            nwords.append(stnew)
+        else:
+            nwords.append(word[::-1])
+    print(" ".join(nwords))
