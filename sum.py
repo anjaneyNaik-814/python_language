a25477acf1673fba90c3460adf53f35837e4d9c4
Anjaney Naik <anjaneynaik05@gmail.com>
2025-05-02 10:07:30 +0530

    initial commit

--- sum.py
+++ sum.py
@@ -1,10 +1,8 @@
-n = int(input("Enter the n number : "))
+input1= input("Enter a number or string: ")
+input2= input("Enter a number or string: ")
 
-sum= 0
-i=0
+if input1.isdigit() and input2.isdigit():
+    print(f"Sum of the two numbers is {int(input1)+int(input2)}")
 
-while i<=n:
-    sum+=i
-    i+=1
-
-print("total sum = ",sum)
+else:
+    print(f"Concatenation of the two strings is {input1+input2}")
